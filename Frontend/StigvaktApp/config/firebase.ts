829be6f4078a2552b7008ae3bc85276f89074847
Linus <Linus.hammarberg@gmail.com>
2025-06-05 16:01:20 +0200

    Removed Auth from Firebase config

--- Frontend/StigvaktApp/config/firebase.ts
+++ Frontend/StigvaktApp/config/firebase.ts
@@ -3,17 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// Replace these with your actual Firebase config values
-const firebaseConfig = {
-  apiKey: "AIzaSyBcTNIhiZelID2uzED3XIPzaazc6K4dNs0",
-  authDomain: "stigvakt-ad307.firebaseapp.com",
-  projectId: "stigvakt-ad307",
-  storageBucket: "stigvakt-ad307.firebasestorage.app",
-  messagingSenderId: "778088096854",
-  appId: "1:778088096854:web:58820a14f5a4fda8b147f7",
-  measurementId: "G-XV56J71ZJ4"
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
